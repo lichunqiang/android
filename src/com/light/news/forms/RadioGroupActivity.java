ab3ff3508a351af82d489a96eb0b0985f3f6dc39
lichunqiang <light-li@hotmail.com>
2016-03-05 12:38:43 +0800

    RadioGroup demo

--- src/com/light/news/forms/RadioGroupActivity.java
+++ src/com/light/news/forms/RadioGroupActivity.java
@@ -4,12 +4,35 @@ import com.light.news.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class RadioGroupActivity extends Activity {
+public class RadioGroupActivity extends Activity implements OnCheckedChangeListener {
+	private RadioGroup rg;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.radio_group_activity);
+		
+		RadioGroup rg = (RadioGroup) findViewById(R.id.radio_group);
+		
+		rg.setOnCheckedChangeListener(this);
+	}
+	@Override
+	public void onCheckedChanged(RadioGroup gr, int checkedId) {
+		
+		switch (checkedId) {
+		case R.id.radio_group_one:
+			Log.i("tag", "篮球被选中");
+			break;
+		case R.id.radio_group_two:
+			Log.i("tag", "足球被选中");
+			break;
+		default:
+			break;
+		}
+		
 	}
 }
